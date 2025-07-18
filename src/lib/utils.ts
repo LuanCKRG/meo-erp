39c509270968c2308a1b67ac4f5f259360327be9
LuanCKRG <luancamposck@gmail.com>
2025-07-18 12:06:51 -0300

    feat(utils): add formatDate function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
+import { format } from "date-fns"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
+}
+
+export function formatDate(dateString: string): string {
+	if (!dateString) return ""
+	const date = new Date(dateString)
+	return format(date, "dd/MM/yyyy")
 }
