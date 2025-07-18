09e18e993b07d0dd793c89cfed16f973354014ab
LuanCKRG <luancamposck@gmail.com>
2025-07-18 12:05:09 -0300

    feat(masks): file for create input masks

---
+++ src/lib/masks.ts
@@ -0,0 +1,38 @@
+function maskCnpj(value: string) {
+	return value
+		.replace(/\D/g, "")
+		.replace(/(\d{2})(\d)/, "$1.$2")
+		.replace(/(\d{3})(\d)/, "$1.$2")
+		.replace(/(\d{3})(\d)/, "$1/$2")
+		.replace(/(\d{4})(\d)/, "$1-$2")
+		.replace(/(-\d{2})\d+?$/, "$1")
+}
+
+function maskPhone(value: string) {
+	let v = value.replace(/\D/g, "")
+	v = v.replace(/^(\d{2})(\d)/g, "($1) $2")
+	if (v.length > 13) {
+		v = v.replace(/(\d{5})(\d)/, "$1-$2")
+	} else {
+		v = v.replace(/(\d{4})(\d)/, "$1-$2")
+	}
+	return v.slice(0, 15)
+}
+
+function maskCep(value: string) {
+	return value
+		.replace(/\D/g, "")
+		.replace(/(\d{5})(\d)/, "$1-$2")
+		.replace(/(-\d{3})\d+?$/, "$1")
+}
+
+function maskCpf(value: string) {
+	return value
+		.replace(/\D/g, "")
+		.replace(/(\d{3})(\d)/, "$1.$2")
+		.replace(/(\d{3})(\d)/, "$1.$2")
+		.replace(/(\d{3})(\d)/, "$1-$2")
+		.replace(/(-\d{2})\d+?$/, "$1")
+}
+
+export { maskCnpj, maskPhone, maskCep, maskCpf }
