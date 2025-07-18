c1eec5971cb3faee12f08c02970d98b17e82c3f4
LuanCKRG <luancamposck@gmail.com>
2025-07-18 12:37:10 -0300

    feat(partner actions): create a barrel file for partner actions

---
+++ src/actions/partners/index.ts
@@ -0,0 +1,5 @@
+import getAllPartners from "@/actions/partners/get-all"
+import getPartnerByCNPJ from "@/actions/partners/get-by-cnpj"
+import registerPartner from "@/actions/partners/register-partner"
+
+export { getAllPartners, getPartnerByCNPJ, registerPartner }
