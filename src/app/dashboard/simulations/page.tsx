516f2e97288a885df14bca32b97a7b2183dece3a
LuanCKRG <luancamposck@gmail.com>
2025-07-30 19:04:21 -0300

    feat(dashboard/simulations: add page to simulations

---
+++ src/app/dashboard/simulations/page.tsx
@@ -0,0 +1,15 @@
+// import EquipmentSelectionForm from "@/components/forms/hiroshima-form"
+import { NewSimulationForm } from "@/components/forms/new-simulation/new-simulation-form"
+// import { NewSimulationForm } from "@/components/forms/new-simulation-form"
+
+const SimulationPages = () => {
+	return (
+		<>
+			{/* <DynamicKitForm /> */}
+			<NewSimulationForm />
+			{/* <EquipmentSelectionForm /> */}
+		</>
+	)
+}
+
+export default SimulationPages
