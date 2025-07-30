909e11fba64edffd04e04fb9106b05842b5f019e
LuanCKRG <luancamposck@gmail.com>
2025-07-30 19:04:58 -0300

    feat(definitions/equipments); add StrutureType to equipments types

--- src/lib/definitions/equipments.d.ts
+++ src/lib/definitions/equipments.d.ts
@@ -3,5 +3,6 @@ import type { Database } from "@/lib/supabase"
 type Equipment = Database["public"]["Tables"]["equipment"]["Row"]
 type EquipmentBrand = Database["public"]["Tables"]["equipment_brands"]["Row"]
 type EquipmentType = Database["public"]["Tables"]["equipment_types"]["Row"]
+type StructureType = Database["public"]["Tables"]["structure_types"]["Row"]
 
-export type { Equipment, EquipmentBrand, EquipmentType }
+export type { Equipment, EquipmentBrand, EquipmentType, StructureType }
