a85b418e900a4f0c024418ae55f6af3eea7c8809
LuanCKRG <luancamposck@gmail.com>
2025-08-02 20:49:50 -0300

    fix(validations/structure-type): change error message  object to string literal

--- src/lib/validations/structure-type.ts
+++ src/lib/validations/structure-type.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 const addStructureTypeSchema = z.object({
-	name: z.string().min(3, { message: "O nome deve ter no mínimo 3 caracteres." }).max(50, { message: "O nome não pode ter mais de 50 caracteres." })
+	name: z.string().min(3, "O nome deve ter no mínimo 3 caracteres.").max(50, "O nome não pode ter mais de 50 caracteres.")
 })
 
 type AddStructureTypeData = z.infer<typeof addStructureTypeSchema>
