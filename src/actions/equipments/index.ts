8333c68ac5791c2f13bd1dcc0ac1a9704f71f855
LuanCKRG <luancamposck@gmail.com>
2025-08-02 20:46:06 -0300

    format(actions/equpments): format imports sort

--- src/actions/equipments/index.ts
+++ src/actions/equipments/index.ts
@@ -1,7 +1,7 @@
+import createStructureType from "@/actions/equipments/create-structure-type"
 import getBrandsByEquipmentType from "@/actions/equipments/get-brands-by-type"
 import getEquipmentTypes from "@/actions/equipments/get-equipment-types"
 import getEquipmentsByBrandAndType from "@/actions/equipments/get-equipments-by-brand-and-type"
 import getStructureTypes from "@/actions/equipments/get-structure-types"
-import createStructureType from "@/actions/equipments/create-structure-type"
 
 export { getBrandsByEquipmentType, getEquipmentsByBrandAndType, getEquipmentTypes, getStructureTypes, createStructureType }
