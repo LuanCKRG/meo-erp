e8a83c34e4365b08b6a64a6996e9f5ba305ba1fa
LuanCKRG <luancamposck@gmail.com>
2025-08-02 21:00:11 -0300

    feat(actions/equipments): add action that create a brand to barrel file

--- src/actions/equipments/index.ts
+++ src/actions/equipments/index.ts
@@ -1,7 +1,8 @@
+import createBrand from "@/actions/equipments/create-brand"
 import createStructureType from "@/actions/equipments/create-structure-type"
 import getBrandsByEquipmentType from "@/actions/equipments/get-brands-by-type"
 import getEquipmentTypes from "@/actions/equipments/get-equipment-types"
 import getEquipmentsByBrandAndType from "@/actions/equipments/get-equipments-by-brand-and-type"
 import getStructureTypes from "@/actions/equipments/get-structure-types"
 
-export { getBrandsByEquipmentType, getEquipmentsByBrandAndType, getEquipmentTypes, getStructureTypes, createStructureType }
+export { getBrandsByEquipmentType, getEquipmentsByBrandAndType, getEquipmentTypes, getStructureTypes, createStructureType, createBrand }
