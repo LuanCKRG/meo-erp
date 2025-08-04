7b010476108509d3714c529402b7aec226953519
LuanCKRG <luancamposck@gmail.com>
2025-08-03 22:57:13 -0300

    feat(validtions): transform all brands to uppercase

--- src/lib/validations/brand.ts
+++ src/lib/validations/brand.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 const addBrandSchema = z.object({
-	name: z.string().min(2, "O nome deve ter no mínimo 2 caracteres.").max(50, "O nome não pode ter mais de 50 caracteres.")
+	name: z.string().toUpperCase().min(2, "O nome deve ter no mínimo 2 caracteres.").max(50, "O nome não pode ter mais de 50 caracteres.")
 })
 
 type AddBrandData = z.infer<typeof addBrandSchema>
