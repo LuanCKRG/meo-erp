9ba1b5a3853c0066b234f8f740f19db2cd369033
LuanCKRG <luancamposck@gmail.com>
2025-08-04 07:21:35 -0300

    feat(actions/auth): create a action to signin user

---
+++ src/actions/auth/sign-in.ts
@@ -0,0 +1,38 @@
+"use server"
+
+import { AuthError } from "@supabase/supabase-js"
+
+import { createClient } from "@/lib/supabase/server"
+import type { SignInData } from "@/lib/validations/auth/sign-in"
+import type { ActionResponse } from "@/types/action-response"
+
+async function signIn(credentials: SignInData): Promise<ActionResponse<{ userId: string }>> {
+	const supabase = await createClient()
+
+	const { data, error } = await supabase.auth.signInWithPassword({
+		email: credentials.email,
+		password: credentials.password
+	})
+
+	if (error) {
+		console.error("Erro de login (Supabase):", error)
+		if (error instanceof AuthError) {
+			return { success: false, message: "Credenciais inválidas. Verifique seu email e senha." }
+		}
+		return { success: false, message: "Ocorreu um erro inesperado. Tente novamente." }
+	}
+
+	if (data.user) {
+		return {
+			success: true,
+			message: "Login bem-sucedido!",
+			data: {
+				userId: data.user.id
+			}
+		}
+	}
+
+	return { success: false, message: "Não foi possível autenticar o usuário." }
+}
+
+export default signIn
