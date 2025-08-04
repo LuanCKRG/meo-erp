54a7ee33d1cb3c1455b29b15eb5e381a64539a4d
LuanCKRG <luancamposck@gmail.com>
2025-08-03 23:00:47 -0300

    feat(definitions/equipments): add EquipmentWithRelations to equipment defiinitions

--- src/lib/definitions/equipments.d.ts
+++ src/lib/definitions/equipments.d.ts
@@ -4,5 +4,9 @@ type Equipment = Database["public"]["Tables"]["equipment"]["Row"]
 type EquipmentBrand = Database["public"]["Tables"]["equipment_brands"]["Row"]
 type EquipmentType = Database["public"]["Tables"]["equipment_types"]["Row"]
 type StructureType = Database["public"]["Tables"]["structure_types"]["Row"]
+type EquipmentWithRelations = Equipment & {
+	equipment_types: { name: string } | null
+	equipment_brands: { name: string } | null
+}
 
-export type { Equipment, EquipmentBrand, EquipmentType, StructureType }
+export type { Equipment, EquipmentBrand, EquipmentType, StructureType, EquipmentWithRelations }
