4c4d5f5d6eae4066a9f4f58a7534ae14852a51cc
LuanCKRG <luancamposck@gmail.com>
2025-08-03 22:58:47 -0300

    feat(constants); add constants of equipment types ids to barrel file

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,5 +1,6 @@
 import brazilianStates from "@/lib/constants/brazilian-states"
 import connectionVoltageTypes from "@/lib/constants/connection-voltage-types"
 import energyProviders from "@/lib/constants/energy-providers"
+import { INVERTER_TYPE_ID, MODULE_TYPE_ID, OTHERS_TYPE_ID } from "@/lib/constants/equipment-types-ids"
 
-export { energyProviders, brazilianStates, connectionVoltageTypes }
+export { energyProviders, brazilianStates, connectionVoltageTypes, INVERTER_TYPE_ID, MODULE_TYPE_ID, OTHERS_TYPE_ID }
