8a2361c7520edb6b8ff82d9c788b4b1557ffbcd1
LuanCKRG <luancamposck@gmail.com>
2025-08-04 01:52:16 -0300

    feat(app-sidebar): create nav-items with navigation items and config

---
+++ src/components/app-sidebar/nav-items.ts
@@ -0,0 +1,27 @@
+import { Briefcase, FileChartLine, Handshake, Home, Package, Settings, Users } from "lucide-react"
+
+const navItems = [
+	{
+		id: 0,
+		title: "Admin",
+		items: [
+			{ title: "Admin Dashboard", url: "/dashboard/admin", icon: Home },
+			{ title: "Gerenciar Dados", url: "/dashboard/admin/data", icon: Package },
+			{ title: "Configurações", url: "/dashboard/admin/settings", icon: Settings },
+			{ title: "Usuários", url: "/dashboard/admin/users", icon: Users }
+		]
+	},
+	{
+		id: 1,
+		title: "Operações",
+		items: [
+			{ title: "Home", url: "/dashboard/home", icon: Handshake },
+			{ title: "Vendedores", url: "/dashboard/sellers", icon: Briefcase },
+			{ title: "Parceiros", url: "/dashboard/partners", icon: Handshake },
+			{ title: "Relatórios", url: "/dashboard/reports", icon: FileChartLine },
+			{ title: "Configurações", url: "/dashboard/settings", icon: Settings }
+		]
+	}
+]
+
+export { navItems }
