002b2dc74578e65d6ab0de5fafbc34703740e96e
LuanCKRG <luancamposck@gmail.com>
2025-08-25 02:32:25 -0300

    feat(partner-actions): export setPartnerActiveStatus function in partners index

--- src/actions/partners/index.ts
+++ src/actions/partners/index.ts
@@ -3,6 +3,7 @@ import getAllPartners from "@/actions/partners/get-all-partners"
 import getPartnerByCNPJ from "@/actions/partners/get-by-cnpj"
 import registerPartner from "@/actions/partners/register-partner"
 import rejectPartner from "@/actions/partners/reject-partner"
+import setPartnerActiveStatus from "@/actions/partners/set-partner-active-status"
 import updatePartner from "@/actions/partners/update-partner"
 
-export { getAllPartners, getPartnerByCNPJ, registerPartner, approvePartner, rejectPartner, updatePartner }
+export { getAllPartners, getPartnerByCNPJ, registerPartner, approvePartner, rejectPartner, updatePartner, setPartnerActiveStatus }
