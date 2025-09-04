1f3ae94b4f422e6989fa7b87003a39aa40e829c6
LuanCKRG <luancamposck@gmail.com>
2025-09-04 12:36:56 -0300

    feat(orders): add updateOrderStatus action for updating order statuses

--- src/actions/orders/index.ts
+++ src/actions/orders/index.ts
@@ -2,5 +2,6 @@ import createOrderFromSimulation from "./create-order-from-simulation"
 import getAllOrders from "./get-all-orders"
 import getOrderById from "./get-order-by-id"
 import updateOrder from "./update-order"
+import updateOrderStatus from "./update-order-status"
 
-export { createOrderFromSimulation, getAllOrders, getOrderById, updateOrder }
+export { createOrderFromSimulation, getAllOrders, getOrderById, updateOrder, updateOrderStatus }
