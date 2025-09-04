ab6e04974393b4e1daca73866e17848812ab4c6d
LuanCKRG <luancamposck@gmail.com>
2025-09-04 11:14:04 -0300

    feat(constants): add base64 simulatio template to constants

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -3,7 +3,18 @@ import connectionVoltageTypes from "@/lib/constants/connection-voltage-types"
 import energyProviders from "@/lib/constants/energy-providers"
 import { INVERTER_TYPE_ID, MODULE_TYPE_ID, OTHERS_TYPE_ID } from "@/lib/constants/equipment-types-ids"
 import PERMISSIONS from "@/lib/constants/permissions"
+import PDF_TEMPLATE_SIMULATION_BASE64 from "@/lib/constants/template-simulation-base44"
 
 type PermissionId = (typeof PERMISSIONS)[number]
 
-export { energyProviders, brazilianStates, connectionVoltageTypes, INVERTER_TYPE_ID, MODULE_TYPE_ID, OTHERS_TYPE_ID, PERMISSIONS, type PermissionId }
+export {
+	energyProviders,
+	brazilianStates,
+	connectionVoltageTypes,
+	INVERTER_TYPE_ID,
+	MODULE_TYPE_ID,
+	OTHERS_TYPE_ID,
+	PERMISSIONS,
+	type PermissionId,
+	PDF_TEMPLATE_SIMULATION_BASE64
+}
