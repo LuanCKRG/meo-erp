114e07ced3f580aec4059dc3ce1180a7a15f9dff
LuanCKRG <luancamposck@gmail.com>
2025-09-11 12:31:34 -0300

    fix(step-5):  change "Enviar Simulação" to "Enviar"

--- src/components/forms/new-simulation/step-5-documents.tsx
+++ src/components/forms/new-simulation/step-5-documents.tsx
@@ -55,7 +55,7 @@ export function SimulationStep5({ onSubmit, onBack }: Step5Props) {
 				</Button>
 				<Button type="button" onClick={onSubmit} disabled={form.formState.isSubmitting}>
 					<Send className="mr-2 h-4 w-4" />
-					Enviar Simulação
+					Enviar
 				</Button>
 			</div>
 		</form>
