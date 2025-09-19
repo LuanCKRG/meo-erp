c467daf82fcfc6b81baa689657a7bdf741b18e69
LuanCKRG <luancamposck@gmail.com>
2025-09-19 18:23:38 -0300

    feat(get-seller-by-user-id): create get-seller-by-user-id and add get-seller-by-user-id to barel file

--- src/actions/sellers/index.ts
+++ src/actions/sellers/index.ts
@@ -5,5 +5,6 @@ import registerSeller from "@/actions/sellers/register-seller"
 import rejectSeller from "@/actions/sellers/reject-seller"
 import updateSeller from "@/actions/sellers/update-seller"
 import setSellerActiveStatus from "@/actions/sellers/set-seller-active-status"
+import getSellerByUserId from "@/actions/sellers/get-seller-by-user-id"
 
-export { registerSeller, getAllSellers, updateSeller, approveSeller, rejectSeller, getAllApprovedSellers, setSellerActiveStatus }
+export { registerSeller, getAllSellers, updateSeller, approveSeller, rejectSeller, getAllApprovedSellers, setSellerActiveStatus, getSellerByUserId }
