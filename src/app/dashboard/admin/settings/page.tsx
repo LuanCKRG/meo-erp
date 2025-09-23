b69fd87b149f7dadc7f9e1417da84a8a0b7b02c6
LuanCKRG <luancamposck@gmail.com>
2025-09-23 07:18:13 -0300

    feat(settings/page): add new interest rate form

--- src/app/dashboard/admin/settings/page.tsx
+++ src/app/dashboard/admin/settings/page.tsx
@@ -1,4 +1,4 @@
-import { InterestRateForm } from "@/components/forms/interest-rate-form"
+import { RateForm } from "@/components/forms/rate-form"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
 const AdminSettingsPage = () => {
@@ -9,15 +9,26 @@ const AdminSettingsPage = () => {
 				<p className="text-muted-foreground">Ajuste os parâmetros globais que afetam os cálculos e simulações.</p>
 			</div>
 
-			<Card>
-				<CardHeader>
-					<CardTitle>Taxa de Juros</CardTitle>
-					<CardDescription>Defina a taxa de juros padrão a ser usada nos cálculos de parcelamento.</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<InterestRateForm />
-				</CardContent>
-			</Card>
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+				<Card>
+					<CardHeader>
+						<CardTitle>Taxa de Juros</CardTitle>
+						<CardDescription>Defina a taxa de juros padrão a ser usada nos cálculos de parcelamento.</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<RateForm rateId="interest_rate" />
+					</CardContent>
+				</Card>
+				<Card>
+					<CardHeader>
+						<CardTitle>Taxa de Serviços</CardTitle>
+						<CardDescription>Defina a taxa de serviços a ser aplicada sobre o subtotal do projeto.</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<RateForm rateId="service_fee" />
+					</CardContent>
+				</Card>
+			</div>
 		</div>
 	)
 }
