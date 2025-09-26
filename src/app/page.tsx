f0b78cc48e4b572d02c49197c527852a166fa9a8
LuanCKRG <luancamposck@gmail.com>
2025-09-26 06:35:55 -0300

    feat: update home to static image on background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,39 @@
 import Image from "next/image"
+import Link from "next/link"
 
 import { AuthFormTabs } from "@/components/auth-form-tabs"
-import { CarouselHome } from "@/components/carousel-home"
 
 const HomePage = () => {
 	return (
-		<div className="grid min-h-svh lg:grid-cols-2">
-			<div className="flex flex-col gap-4 p-6 md:p-10">
-				{/* <div className="flex justify-center gap-2 md:justify-start">
-					<a href="#" className="flex items-center gap-2 font-medium">
-								Acme Inc.
-							</a>
-				</div> */}
-				<div className="flex flex-1 items-center justify-center">
-					<div className="w-full">
-						<Image src="/logo.png" alt="MEO Leasing" width={400} height={300} className="mx-auto" />
-						<AuthFormTabs />
-					</div>
+		<div className="relative  flex min-h-svh flex-col items-center p-6 md:p-10">
+			{/* Imagem de fundo ocupando a tela inteira */}
+			<Image
+				src="/banner-3.jpeg" // Substitua pelo caminho da sua imagem
+				alt="Background"
+				fill
+				className="object-cover z-[-1]"
+				priority // Opcional: carrega a imagem com prioridade
+				quality={75} // Opcional: ajusta a qualidade da imagem
+			/>
+
+			{/* Conteúdo original */}
+			<div className="w-full max-w-sm md:max-w-md relative z-10">
+				<Image src="/logo-azul-branco.png" alt="MEO Leasing" width={300} height={200} className="w-full" />
+				<AuthFormTabs />
+				<div className="text-white mt-6 text-center text-xs text-balance">
+					<p>
+						Ao continuar, você concorda com nossos{" "}
+						<Link href="#" className="underline underline-offset-4 hover:text-primary">
+							Termos de Serviço
+						</Link>{" "}
+						e{" "}
+						<Link href="#" className="underline underline-offset-4 hover:text-primary">
+							Política de Privacidade
+						</Link>
+						.
+					</p>
 				</div>
 			</div>
-			<CarouselHome />
 		</div>
 	)
 }
