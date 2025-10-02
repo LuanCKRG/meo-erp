fcb80ddf59d5d1c997c10586ff82e4ed96bb9e07
LuanCKRG <luancamposck@gmail.com>
2025-10-02 08:25:11 -0300

    fix(dashboard home): add title and border radius

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,18 @@
+import { getCurrentUser } from "@/actions/auth"
 import { BannerCarousel } from "@/components/banner-carousel"
+import { getFirstAndLastName } from "@/lib/utils"
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+	const userData = await getCurrentUser()
+
 	return (
-		<div>
+		<div className="">
+			<h3 className="text-2xl">
+				Bem-vindo, <span className="font-semibold">{getFirstAndLastName(userData.name)}</span>!
+			</h3>
+			<p className="mb-3 md:mb-6">
+				Seja bem-vindo a <span className="font-semibold text-primary">MEO Leasing</span>. Confira as nossas novidades abaixo:
+			</p>
 			<BannerCarousel />
 		</div>
 	)
