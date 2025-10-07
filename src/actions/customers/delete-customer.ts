fbb4a6d3ccdfe62c3db90ab2b71c30a564405c29
LuanCKRG <luancamposck@gmail.com>
2025-10-07 14:49:57 -0300

    feat(delete-customer): add a if error code 23503 to handle fk deleting

--- src/actions/customers/delete-customer.ts
+++ src/actions/customers/delete-customer.ts
@@ -14,7 +14,16 @@ async function deleteCustomer(customerId: string): Promise<ActionResponse<{ cust
 		const { error } = await supabase.from("customers").delete().eq("id", customerId)
 
 		if (error) {
-			console.error("Erro ao deletar cliente (Supabase):", error)
+			console.error("Erro ao deletar customer (Supabase):", error)
+
+			if (error.code === "23503") {
+				// Foreign key violation
+				return {
+					success: false,
+					message: "Não é possível deletar este cliente, pois ele está associado a uma ou mais simulações ou pedidos."
+				}
+			}
+
 			return {
 				success: false,
 				message: "Erro ao deletar o cliente. Por favor, tente novamente."
