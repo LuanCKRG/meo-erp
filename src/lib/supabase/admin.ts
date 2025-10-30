10bec72cfdbb2eb76be3d9f5380c1089d5e3f4ed
LuanCKRG <luancamposck@gmail.com>
2025-10-30 08:00:20 -0300

    fix(supabase): use service role to supabase admin

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -9,7 +9,7 @@ import type { Database } from "@/lib/definitions/supabase"
 
 export function createAdminClient() {
 	const supabaseUrl = process.env.SUPABASE_URL
-	const supabaseServiceRoleKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+	const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
 	if (!supabaseUrl || !supabaseServiceRoleKey) {
 		throw new Error("Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY environment variable")
